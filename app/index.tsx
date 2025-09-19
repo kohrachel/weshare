ad631739500b45582a569918fb46a8793e46a9ba
Emma Reid <78507456+23reide@users.noreply.github.com>
2025-09-19 13:54:11 -0500

    Basic navigation Implemented so all pages can be reached from first page

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,13 @@
 import { Text, View } from "react-native";
+import { useRouter } from 'expo-router';
+import { Button } from 'react-native';
 
-// Main screen
+// First screen - will be deleted in final product
+// Navigation info: docs.expo.dev/router/basics/navigation/
 export default function Index() {
+  //
+  const router = useRouter();
+
   return (
     <View
       style={{
@@ -11,6 +17,11 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button title="Feed" onPress={() => router.navigate('/login')} />
+      <Button title="Post a Ride" onPress={() => router.navigate('/createRide')} />
+      <Button title="Edit Profile" onPress={() => router.navigate('/editProfile')} />
+      <Button title="Login" onPress={() => router.navigate('/login')} />
+      <Button title="RSVP" onPress={() => router.navigate('/rsvp')} />
     </View>
   );
 }
