e6d830691feedc4e04e5a2c63b13519f843232a1
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-09-25 00:11:04 -0500

    update imports

--- app/createRide.tsx
+++ app/createRide.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import Input from "@/components/Input";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
