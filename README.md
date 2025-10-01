efe87f1931cd785b6c9625157bb15782c813eb00
Emma Reid <78507456+23reide@users.noreply.github.com>
2025-10-01 11:09:06 -0500

    Firebase successfully pushing data

--- README.md
+++ README.md
@@ -142,7 +142,10 @@ Join our community of developers creating universal apps.
 
 ## Firebase
 Firebase project is named WeShare. (Firestore)[https://console.firebase.google.com/u/0/project/weshare-c1834/firestore/databases/-default-/data] is built within that Firebase project.
-(Primary docs)[https://firebase.google.com/docs/firestore/quickstart#node.js]
+(Official Firebase docs)[https://firebase.google.com/docs/firestore/quickstart#node.js] - code examples
+React Native Projects are considered Web modular API projects.
+(ReactNative + Expo specific docs)[https://rnfirebase.io/] - helpful for setup
+(Expo Specific Docs)[https://docs.expo.dev/guides/using-firebase/] - explain more about firebaseConfig.js
 
 On the free plan, we are currently [limited](https://firebase.google.com/pricing?authuser=0&_gl=1*snijf2*_ga*MjA2NDUwMjc0Ny4xNzU5MjUyMjE5*_ga_CW55HF8NVT*czE3NTkyNzExNTYkbzIkZzEkdDE3NTkyNzE0MTckajExJGwwJGgw)
 but should have enough free access for starting out.
--- app/createRide.tsx
+++ app/createRide.tsx
@@ -1,6 +1,27 @@
 import { ButtonGreen } from "@/components/button-green";
 import Input from "@/components/Input";
 import { StyleSheet, Text, View } from "react-native";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "@/firebaseConfig";
+
+async function storeRide(dest: string, date: string, time: string, meetLoc: string, numberPpl: number) {
+  try {
+    await addDoc(collection(db, "rides"), {
+      destination: dest,
+      date: date,
+      time: time,
+      meetLoc: meetLoc,
+      numberPpl: numberPpl
+    });
+
+    console.log("Success!");
+    return true;
+
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
 
 export default function Index() {
   return (
@@ -25,7 +46,7 @@ export default function Index() {
         ></Input>
         <Input label={"How many people?"} defaultValue={"e.g. 4"}></Input>
       </View>
-      <ButtonGreen title="Create New Ride" onPress={() => {}} />
+      <ButtonGreen title="Create New Ride" onPress={() => storeRide('BNA', '1/1/1', '8pm', 'Commons', 4)} />
     </View>
   );
 }
