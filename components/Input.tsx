c0f6ba099ce66f3a0f338f0fabaed63461e8ee2f
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-10-20 19:13:14 -0500

    remove uneccessary logic from Input

--- components/Input.tsx
+++ components/Input.tsx
@@ -12,46 +12,13 @@ export default function Input({ label, defaultValue, inputType }: InputProps) {
   return (
     <View style={styles.inputWrapper}>
       {label && <Text style={styles.inputLabel}>{label}</Text>}
-      {inputType === "time" ? (
-        <View style={styles.inputTimeRow}>
-          <TextInput
-            style={styles.inputCell}
-            value={value}
-            placeholder={"Jan"}
-            placeholderTextColor="#999"
-            onChangeText={setValue}
-          />
-          <TextInput
-            style={styles.inputCell}
-            value={value}
-            placeholder={"01"}
-            placeholderTextColor="#999"
-            onChangeText={setValue}
-          />
-          <TextInput
-            style={styles.inputCell}
-            value={value}
-            placeholder={"2000"}
-            placeholderTextColor="#999"
-            onChangeText={setValue}
-          />
-          <TextInput
-            style={styles.inputCell}
-            value={value}
-            placeholder={"8pm"}
-            placeholderTextColor="#999"
-            onChangeText={setValue}
-          />
-        </View>
-      ) : (
-        <TextInput
-          style={styles.inputBox}
-          value={value}
-          placeholder={defaultValue ?? ""}
-          placeholderTextColor="#999"
-          onChangeText={setValue}
-        />
-      )}
+      <TextInput
+        style={styles.inputBox}
+        value={value}
+        placeholder={defaultValue ?? ""}
+        placeholderTextColor="#999"
+        onChangeText={setValue}
+      />
     </View>
   );
 }
@@ -79,23 +46,5 @@ const styles = StyleSheet.create({
     color: "#e7e7e7",
     fontFamily: "Inter_700Bold",
     fontSize: 16,
-  },
-  inputTimeRow: {
-    flexDirection: "row",
-    gap: 10,
-  },
-  inputCell: {
-    height: "auto",
-    paddingHorizontal: 10,
-    paddingVertical: 12,
-    borderRadius: 10,
-    outlineColor: "#e7e7e7",
-    outlineStyle: "solid",
-    outlineWidth: 1,
-    color: "#e7e7e7",
-    fontFamily: "Inter_400Regular",
-    fontSize: 14,
-    width: 60,
-    textAlign: "center",
   },
 });
