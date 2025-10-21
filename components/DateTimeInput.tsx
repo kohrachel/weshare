a6905a31d6fd337366ddc434645528405c80a24d
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-10-20 19:04:58 -0500

    copy styles over from the Input component

--- components/DateTimeInput.tsx
+++ components/DateTimeInput.tsx
@@ -1,1 +1,17 @@
 import RNDateTimePicker from "@react-native-community/datetimepicker";
+const styles = StyleSheet.create({
+  inputWrapper: {
+    flexDirection: "column",
+    gap: 15,
+    width: "100%",
+  },
+  dateTimeCells: {
+    flexDirection: "row",
+    gap: 10,
+  },
+  inputLabel: {
+    color: "#e7e7e7",
+    fontFamily: "Inter_700Bold",
+    fontSize: 16,
+  },
+});
